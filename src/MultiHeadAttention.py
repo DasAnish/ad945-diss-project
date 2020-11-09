5364ad1ccb268015303700dbc2ab861b3517b2a4
DasAnish <ad945@cam.ac.uk>
2020-11-09 17:43:55 +0000

    Basic components complete

---
+++ src/MultiHeadAttention.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from math import sqrt
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_head, model_dim, dropout=0.1):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.heads = num_head
+        self.d_k = model_dim // num_head
+        self.dropout = nn.Dropout(dropout)
+
+        self.w_key = nn.Linear(model_dim, model_dim)
+        self.w_query = nn.Linear(model_dim, model_dim)
+        self.w_value = nn.Linear(model_dim, model_dim)
+
+        self.final_layer = nn.Linear(model_dim, model_dim)
+
+    def forward(self, x, mask = None):
+        # X has the shape batch_size, seq_len, model_dim
+
+        batch_size = x.size(0)
+        keys = self.w_key(x).view(batch_size, -1, self.heads, self.d_k)
+        queries = self.w_query(x).view(batch_size, -1, self.heads, self.d_k)
+        values = self.w_value(x).view(batch_size, -1, self.heads, self.d_k)
+
+        # we have batch_size, seq_len, heads, d_k tensor
+
+        keys.transpose_(1, 2)
+        queries.transpose_(1, 2)
+        values.transpose_(1, 2)
+
+        # after transposing we have batch_size, heads, seq_len, d_k tensor
+
+        interim_result = torch.matmul(queries, keys.transpose(-2, -1)) / sqrt(self.d_k)
+
+        # TODO: add the mask operation
+
+        interim_result = F.softmax(interim_result, dim=-1)
+
+        if self.dropout is not None:
+            interim_result = self.dropout(interim_result)
+
+        interim_result = torch.matmul(interim_result, values)
+
+        concatenated_result = interim_result.transpose(1, 2).contiguous().view(batch_size, -1, self.model_dim)
+
+        final_result = self.final_layer(concatenated_result)
+
+        return final_result
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 from torch.autograd import Variable
 
 
-class WordEmbedder(nn.Module):
+class WordEmbeddings(nn.Module):
 
     def __init__(self, size_of_vocab, embedding_dim):
         super().__init__()
@@ -37,5 +37,37 @@ class PositionalEncoding(nn.Module):
 
         x = x + Variable(self.position_vector[:, :sequence_length], requires_grad=False)
 
+
+class Norm(nn.Module):
+    def __init__(self, model_dim, eps = 1e-5):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.eps = eps
+
+        # two learnable parameters to get better normalizations
+        self.alpha = nn.Parameter(torch.ones(self.model_dim))
+        self.bias = nn.Parameter(torch.zeros(self.model_dim))
+
+    def forward(self, x):
+        norm = self.alpha * (x - x.mean(dim=-1, keepdim=True)) / (x.std(dim=-1, keepdim=True) + self.eps) + self.bias
+
+        return norm
+
+
+class FeedForward(nn.Module):
+    def __init__(self, model_dim, d_ff = 2048, dropout=0.1):
+        super().__init__()
+
+        self.fc1 = nn.Linear(model_dim, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.fc2 = nn.Linear(d_ff, model_dim)
+
+    def forward(self, x):
+
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.fc2(x)
+
+        return x
 
 
